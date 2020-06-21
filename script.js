eb2a2f8e4fa4177b35f383b62e7ca7a34c80c4e4
Master Chief <kemar7@live.com>
2020-06-21 17:37:08 -0400

    add global variables

---
+++ script.js
@@ -0,0 +1,14 @@
+// Add global variables
+  
+var abcUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var abcUpperArr = abcUpper.split("");
+var abcLower = "abcdefghijklmnopqrstuvwxyz";
+var abcLowerArr = abcLower.split("");
+var num = "0123456789";
+var numArr = num.split("");
+var sym = "!#$%&\()*+,-./:;<=>?@^[\\]^_`{|}~";
+var symArr = sym.split("");
+
+//  Prompt message for user
+
+window.alert("Welcome! Please click 'Generate password' to start!");
